d6401848017c3516c800a41673abe6318e6143cf
ev33 <x145789@naver.com>
2025-03-26 00:57:41 +0900

    폰트 설정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,14 @@
 </div>
 
 <style>
+	@font-face {
+		font-family: "PyeongChangPeace-Bold";
+		src: url("https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-02@1.0/PyeongChangPeace-Light.woff2")
+			format("woff2");
+		font-weight: 700;
+		font-style: normal;
+	}
+
 	#introPage {
 		min-height: calc(100vh - 179.59px);
 		height: 1500px;
