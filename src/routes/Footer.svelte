c62c6dc5da903267e86318f790882c493339bb7c
ev33 <x145789@naver.com>
2025-04-18 22:29:00 +0900

    통신판매업 신고 번호 추가

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -3,7 +3,7 @@
 		<li><span>원바이트웍스</span> 대표 최태호</li>
 		<li><span>사업자등록번호</span> 455-30-01623</li>
 		<li><span>이메일</span> cs@tarotpack.com</li>
-		<!-- <li><span>통신판매업신고번호</span> 제2025-대한민국-1234호</li> -->
+		<li><span>통신판매업신고번호</span> 제2025-경기하남-0740호</li>
 		<li><span>주소</span> 경기도 하남시 조정대로 85, 205-3호(풍산동, 마이움트라이스타)</li>
 	</ul>
 	<span id="copyright">ⓒ Tarotpack. All rights reserved.</span>
